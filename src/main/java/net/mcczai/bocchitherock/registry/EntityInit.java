9e7a54a2754e3e84eaf0527723b69be59971df60
mcczai <hjcz903cyd@163.com>
2023-07-14 22:41:44 +0800

    依赖添加

--- src/main/java/net/mcczai/bocchitherock/registry/EntityInit.java
+++ src/main/java/net/mcczai/bocchitherock/registry/EntityInit.java
@@ -5,7 +5,6 @@ import net.mcczai.bocchitherock.entity.client.DrumEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -13,7 +12,8 @@ import net.minecraftforge.registries.RegistryObject;
 public class EntityInit {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, bocchitherock.ID);
 
-    public static final RegistryObject<EntityType<DrumEntity>> DRUM_SIT = register("drum_set", EntityType.Builder.<DrumEntity>of((type, world) -> new DrumEntity(world), MobCategory.MISC).sized(0.0F, 0.0F).setCustomClientFactory((spawnEntity, world) -> new DrumEntity(world)));
+    public static final RegistryObject<EntityType<DrumEntity>> DRUM_SIT = register("drum_set", EntityType.Builder.<DrumEntity>of((type, world) ->
+            new DrumEntity(world), MobCategory.MISC).sized(0.0F, 0.0F).setCustomClientFactory((spawnEntity, world) -> new DrumEntity(world)).fireImmune());
 
     private static <T extends Entity> RegistryObject<EntityType<T>> register(String name, EntityType.Builder<T> builder)
     {
